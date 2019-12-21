497fbc3c56eda6b14d9267f68f3118179b26da8e
Josh Frechette <josh.f.design@gmail.com>
2019-12-21 14:33:57 -0500

    SQL files created.

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
 -- create database and table schema here
+DROP DATABASE IF EXISTS resto_db;
+
+CREATE DATABASE resto_db;
+ 
+USE resto_db;
+
+CREATE TABLE burger (
+    id int AUTO_INCREMENT NOT NULL,
+    choice TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,11 @@
 -- insert burgers here
+INSERT INTO burger (choice)
+VALUES
+("Fish Filet"),
+("Royale with Cheese"),
+("Krusty Burger"),
+("Poutine on the Ritz Burger"),
+("Poblano Picasso Burger"),
+("Eggers can't be Cheesers Burger"),
+("Beets of Burden Burger"),
+("Last of the Mo-jicama Burger");
