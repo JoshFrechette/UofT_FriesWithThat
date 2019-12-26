8024dd143133987053b205445c31dbbdb575ac9a
Josh Frechette <josh.f.design@gmail.com>
2019-12-26 15:04:06 -0500

    Updated CSS. jQuery updated. Handlebars files updated.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,46 @@
 // jquery code goes here
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-state").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEat = $(this).data("newBurger");
+  
+      var newEatState = {
+        devoured: newEat
+      };
+  
+      // Update the burger state to "devoured"
+      $.ajax("/api/burger/" + id, {
+        type: "UPDATE",
+        data: newEatState
+      }).then(
+        function() {
+          console.log("changed burger state to", newEat);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[choice=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
