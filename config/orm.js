89e5588bc43ba233098f2eefa2d1b2a3989a3ab7
Josh Frechette <josh.f.design@gmail.com>
2019-12-28 12:27:20 -0500

    Improved UI thru CSS, adjusting some handlebar elements.

--- config/orm.js
+++ config/orm.js
@@ -4,41 +4,41 @@
 var connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
-    var arr = [];
+  var arr = [];
 
-    for (var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
 }
 
 function objToSQL(ob) {
-    var arr = [];
-    for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf("") >= 0) {
-                value = "'" + value + "'";
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf("") >= 0) {
+        value = "'" + value + "'";
 
-            }
-            arr.push(key + "=" + value)
-        }
+      }
+      arr.push(key + "=" + value)
     }
-    return arr.toString();
+  }
+  return arr.toString();
 }
 
 var orm = {
-    all: function(tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-            if (err){
-                throw err;
-            }
-            cb(result);
-        
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+
     });
-},
-create: function(table, cols, vals, cb) {
+  },
+  create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
     queryString += " (";
@@ -50,7 +50,7 @@ create: function(table, cols, vals, cb) {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(queryString, vals, function (err, result) {
       if (err) {
         throw err;
       }
@@ -59,7 +59,7 @@ create: function(table, cols, vals, cb) {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
+  update: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
@@ -68,7 +68,7 @@ create: function(table, cols, vals, cb) {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function(err, result) {
+    connection.query(queryString, function (err, result) {
       if (err) {
         throw err;
       }
